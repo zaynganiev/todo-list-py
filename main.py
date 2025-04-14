86be9f1c45044fbedb27b6382692fd639f5a432e
zaynganiev <84282742+zaynganiev@users.noreply.github.com>
2025-04-14 06:35:55 -0400

    default-args updated

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-def get_todos(filepath):
+def get_todos(filepath="todos.txt"):
+    """ Read a text file and return the list of 
+     to-do items. 
+    """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath="todos.txt"):
+    """ Write the to-do items list in the text file. """
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
@@ -15,15 +19,15 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
         
-        todos = get_todos("todos.txt")
+        todos = get_todos()
 
         todos.append(todo + '\n') 
 
-        write_todos("todos.txt", todos)
+        write_todos(todos)
 
     elif user_action.startswith("show"):
 
-        todos = get_todos("todos.txt")
+        todos = get_todos()
 
         # new_todos = [item.strip('\n') for item in todos]
 
@@ -37,12 +41,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
             
-            todos = get_todos("todos.txt")
+            todos = get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            write_todos("todos.txt", todos)
+            write_todos(todos)
         except ValueError:
             print("Your command is not valid.")
             continue
@@ -51,12 +55,12 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos("todos.txt")
+            todos = get_todos()
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos("todos.txt", todos)
+            write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
