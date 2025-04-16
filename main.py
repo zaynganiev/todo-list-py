2ed364bf09b78f4c77165d32a9915bd92cb8bfd4
zaynganiev <84282742+zaynganiev@users.noreply.github.com>
2025-04-16 06:39:58 -0400

    Local Modules

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-def get_todos(filepath="todos.txt"):
-    """ Read a text file and return the list of 
-     to-do items. 
-    """
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-def write_todos(todos_arg, filepath="todos.txt"):
-    """ Write the to-do items list in the text file. """
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
+from functions import get_todos, write_todos
 
 while True:
     # Get user input and strip space chars from it
