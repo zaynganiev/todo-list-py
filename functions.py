2e7cee29a604524e4646915017eceea17948d018
zaynganiev <84282742+zaynganiev@users.noreply.github.com>
2025-04-19 14:06:28 -0400

    Add FILEPATH for DB

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
     """ Read a text file and return the list of 
      to-do items. 
     """
@@ -6,7 +8,11 @@ def get_todos(filepath="todos.txt"):
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the text file. """
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
+
+    if __name__ == "__main__":
+        print("Hello")
+        print(get_todos())
