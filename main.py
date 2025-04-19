f17a8f302ec1e712d72b7e3ce5299c5f95e85288
zaynganiev <84282742+zaynganiev@users.noreply.github.com>
2025-04-19 14:01:27 -0400

    Add "time" module

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from functions import get_todos, write_todos
+import time
+
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("It is", now)
 
 while True:
     # Get user input and strip space chars from it
