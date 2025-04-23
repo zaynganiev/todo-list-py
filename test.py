b8ee72be067b3533d090b5d593369ca682a0ff0e
zaynganiev <84282742+zaynganiev@users.noreply.github.com>
2025-04-23 19:32:28 -0400

    #

--- test.py
+++ test.py
@@ -1,5 +1,28 @@
-import webbrowser
+#User url-search program
+"""import webbrowser
 
 user_term = input("Enter a term: ").replace(" ", "+")
 
-webbrowser.open("https://www.google.com/search?q=" + user_term)
+webbrowser.open("https://www.google.com/search?q=" + user_term)"""
+
+
+#File converter program
+"""import FreeSimpleGUI as sg 
+
+label1 = sg.Text("Select files to compress: ")
+input1 = sg.Input()
+choose_button1 = sg.FilesBrowse("Choose")
+
+label2 = sg.Text("Select destination folder: ")
+input2 = sg.Input()
+choose_button2 = sg.FolderBrowse("Choose")
+
+compress_button = sg.Button("Compress")
+
+window = sg.Window("File Compressor", 
+                   layout=[[label1, input1, choose_button1],
+                           [label2, input2, choose_button2],
+                            [compress_button] ])
+window.read()
+window.close()
+"""
