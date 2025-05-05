082b3a77fefddcf67c7c2cc598a9499ac8d63b64
zaynganiev <84282742+zaynganiev@users.noreply.github.com>
2025-05-05 06:42:56 -0400

    Implement the complete and exit buttons

--- gui.py
+++ gui.py
@@ -8,9 +8,14 @@ add_button = FreeSimpleGUI.Button("Add")
 list_box = FreeSimpleGUI.Listbox(values=functions.get_todos(), key='todos', 
                                  enable_events=True, size=[45, 10])
 edit_button = FreeSimpleGUI.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = FreeSimpleGUI.Window('My To-Do App',
-                               layout=[[label], [input_box, add_button], [list_box, edit_button]], 
+                               layout=[[label],
+                                        [input_box, add_button],
+                                        [list_box, edit_button, complete_button],
+                                        [exit_button]], 
                                font=('Helvetica', 20))
 
 while True:
@@ -32,6 +37,17 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+
+        case "Exit":
+            break
 
         case 'todos':
             window['todo'].update(value=values['todos'][0])
