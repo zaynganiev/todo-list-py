bf6f423144268f1126159a12514695f6a1cd2fd6
zaynganiev <84282742+zaynganiev@users.noreply.github.com>
2025-05-05 07:05:00 -0400

    Improved UI and set a local time feature

--- gui.py
+++ gui.py
@@ -1,10 +1,14 @@
 import FreeSimpleGUI.window
 import functions
 import FreeSimpleGUI
+import time
 
+FreeSimpleGUI.theme("Black")
+
+clock = FreeSimpleGUI.Text('', key='clock')
 label = FreeSimpleGUI.Text("Type in a to-do")
 input_box = FreeSimpleGUI.InputText(tooltip="Enter todo", key="todo")
-add_button = FreeSimpleGUI.Button("Add")
+add_button = FreeSimpleGUI.Button("Add", size=10)
 list_box = FreeSimpleGUI.Listbox(values=functions.get_todos(), key='todos', 
                                  enable_events=True, size=[45, 10])
 edit_button = FreeSimpleGUI.Button("Edit")
@@ -12,14 +16,16 @@ complete_button = FreeSimpleGUI.Button("Complete")
 exit_button = FreeSimpleGUI.Button("Exit")
 
 window = FreeSimpleGUI.Window('My To-Do App',
-                               layout=[[label],
+                               layout=[[clock],
+                                        [label],
                                         [input_box, add_button],
                                         [list_box, edit_button, complete_button],
                                         [exit_button]], 
                                font=('Helvetica', 20))
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=200)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -29,22 +35,28 @@ while True:
             window['todos'].update(values=todos)
 
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
 
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                FreeSimpleGUI.popup("Please select an item first.", font=('Helvetica', 20))
 
         case "Complete":
-            todo_to_complete = values['todos'][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value='')
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                FreeSimpleGUI.popup("Please select an item first.", font=('Helvetica', 20))
 
         case "Exit":
             break
