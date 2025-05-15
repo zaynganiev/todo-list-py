c987a6f50cc28cc3918e75296ea0adbd910aec3e
zaynganiev <84282742+zaynganiev@users.noreply.github.com>
2025-05-15 18:13:43 -0400

    Finish the deleting function in the web app

--- web.py
+++ web.py
@@ -1,19 +1,25 @@
 import streamlit as st
 import functions
 
+todos = functions.get_todos()
+
 def add_todo():
-    todo = st.session_state["new_todo"]
-    print(todo)
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
 
 todos = functions.get_todos()
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Add new todo...", 
               on_change=add_todo, key='new_todo')
-
-st.session_statew
